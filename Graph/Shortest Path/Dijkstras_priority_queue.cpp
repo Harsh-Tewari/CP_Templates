a8a7bf24dbf324ba56e338ba1d056df8f4b88bd9
Harsh Dutta Tewari <103374169+Harsh-Tewari@users.noreply.github.com>
2023-05-09 19:39:07 +0530

    Dijkstras

---
+++ Graph/Shortest Path/Dijkstras_priority_queue.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pii pair<int, int>
+vector<int> dijkstra(int n, vector<pair<int, int>> adj[], int src)
+{
+    priority_queue<pii, vector<pii>, greater<pii>> pq;
+    vector<int> distance(n, INT_MAX);
+    distance[src] = 0;
+    pq.push({0, src});
+    while (!pq.empty())
+    {
+        int node = pq.top().second;
+        int nodeVal = pq.top().first;
+        pq.pop();
+        for (auto &it : adj[node])
+        {
+            int edgeWt = it.second;
+            int v = it.first;
+            if (distance[v] > nodeVal + edgeWt)
+            {
+                distance[v] = nodeVal + edgeWt;
+                pq.push({distance[v], v});
+            }
+        }
+    }
+    return distance;
+}
