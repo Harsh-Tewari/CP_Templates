8a1ea6879c0b6993959146bc0418588202d8fe9e
Harsh Dutta Tewari <103374169+Harsh-Tewari@users.noreply.github.com>
2023-06-15 15:47:39 +0530

    Max Xor Using Trie

---
+++ Trie/Max_XOR_using_Trie.cpp
@@ -0,0 +1,62 @@
+// Stores each no. in form of 32 bits
+// bits can be changed by changing the no.
+// xorMax can give us max xor with a no. form the array inserted in a trie with the target no.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+    node *links[2];
+    bool ending = false;
+};
+class Trie
+{
+public:
+    node *root;
+    Trie()
+    {
+        root = new node();
+    }
+
+    void insert(int s)
+    {
+        node *temp = root;
+        int x;
+        for (int i = 31; i >= 0; i--)
+        {
+            x = ((s >> i) & 1);
+            if (temp->links[x] != NULL)
+            {
+                temp = temp->links[x];
+            }
+            else
+            {
+                temp->links[x] = new node();
+                temp = temp->links[x];
+            }
+        }
+        temp->ending = true;
+    }
+
+    int xorMax(int num)
+    {
+        node *temp = root;
+        int x;
+        int ans = 0;
+        for (int i = 31; i >= 0; i--)
+        {
+            x = ((num >> i) & 1);
+            if (temp->links[1 - x] != NULL)
+            {
+                temp = temp->links[1 - x];
+                ans += (1 << i);
+            }
+            else
+            {
+                temp = temp->links[x];
+            }
+        }
+        return ans;
+    }
+};
